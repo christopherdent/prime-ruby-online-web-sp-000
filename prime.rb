f1113b0fdba76bec0ae35af20657a2438ddaee54
Christopher Dent <christopherdent01@gmail.com>
2019-07-30 01:35:21 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,11 +1,13 @@
-
-
-def prime?(n)
-  
-  if n%2 == 0 
-    return true 
-  
-  end 
-
-end 
-  
+def get_prime_no_upto(number)
+  start = 2
+  primes = (start..number).to_a
+  (start..number).each do |no|
+    (start..no).each do |num|
+      if ( no % num  == 0) && num != no
+        primes.delete(no)
+        break
+      end
+    end
+  end
+  primes
+end
