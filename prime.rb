88fb25dd98329bf64a4b34f74c87528db05016af
Christopher Dent <christopherdent01@gmail.com>
2019-07-30 01:37:38 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,6 +1,6 @@
 def prime?(number)
   start = 2
-  primes = (start..number).to_a
+  primes = (start..number)
   (start..number).each do |no|
     (start..no).each do |num|
       if ( no % num  == 0) && num != no
