dc8821e7c391e67798b270b92e5b58db88b215a4
Christopher Dent <christopherdent01@gmail.com>
2019-07-30 01:38:08 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -2,12 +2,12 @@ def prime?(number)
   start = 2
   primes = (start..number)
   (start..number).each do |no|
-    (start..no).each do |num|
+    
       if ( no % num  == 0) && num != no
         primes.delete(no)
         break
       end
     end
-  end
-  primes
+  
+  
 end
