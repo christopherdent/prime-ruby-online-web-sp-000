109705dd3695560a86151fd902f6df26ff80078d
Christopher Dent <christopherdent01@gmail.com>
2019-07-30 01:28:35 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -2,16 +2,6 @@
 
 def prime?(n)
   
-  if n < 3
-        return n > 1
-     elsif n mod 2 = 0 or n mod 3 = 0
-        return false
-     let i ← 5
-     while i * i ≤ n
-        if n mod i = 0 or n mod (i + 2) = 0
-            return false
-        i ← i + 6
-     return true
-  
+
 end 
   
